2723c79a5e9021a51bfc4f940a95ff4aaea2947f
Darkyenus <darkyenus@gmail.com>
2017-01-18 15:56:31 +0100

    Pre Quartus
    Took 2 minutes

--- hw/src/blocpu_fpga_runner.v
+++ hw/src/blocpu_fpga_runner.v
@@ -40,65 +40,26 @@ module blocpu_fpga_runner(
 	reg [15:0] in_instruction_address;
 	reg in_instruction_write;
 
-	blocpu_core core (in_running, in_reset, out_running, out_reset, in_instruction, in_instruction_address, in_instruction_write);
+	wire [7:0] out_output;
+    wire out_output_trigger;
+    reg [7:0] in_input;
+
+	blocpu_core core (in_running, in_reset, out_running, out_reset,
+						in_instruction, in_instruction_address, in_instruction_write,
+						out_output, out_output_trigger, in_input);
 
 	always @(negedge KEY[0]) begin
 		in_reset = 1;
 		
-		in_instruction_write = 0;
-        in_instruction = 12'b100000000000;
-        in_instruction_address = 0;
-        in_instruction_write = 1;
-        in_instruction_write = 0;
-        in_instruction = 12'b100101000000;
-        in_instruction_address = 1;
-        in_instruction_write = 1;
-        in_instruction_write = 0;
-        in_instruction = 12'b101100001001;
-        in_instruction_address = 2;
-        in_instruction_write = 1;
-        in_instruction_write = 0;
-        in_instruction = 12'b110000000101;
-        in_instruction_address = 3;
-        in_instruction_write = 1;
-        in_instruction_write = 0;
-        in_instruction = 12'b110100000001;
-        in_instruction_address = 4;
-        in_instruction_write = 1;
-        in_instruction_write = 0;
-        in_instruction = 12'b011010000001;
-        in_instruction_address = 5;
-        in_instruction_write = 1;
+		//Instruction begin
 
-        in_instruction_write = 0;
-        in_instruction = 12'b001100010011;
-        in_instruction_address = 6;
-        in_instruction_write = 1;
-
-        in_instruction_write = 0;
-        in_instruction = 12'b010000000101;
-        in_instruction_address = 7;
-        in_instruction_write = 1;
-        in_instruction_write = 0;
-        in_instruction = 12'b001100001100;
-        in_instruction_address = 8;
-        in_instruction_write = 1;
-        in_instruction_write = 0;
-        in_instruction = 12'b111011111111;
-        in_instruction_address = 9;
-        in_instruction_write = 1;
-        in_instruction_write = 0;
-        in_instruction = 12'b111111111111;
-        in_instruction_address = 10;
-        in_instruction_write = 1;
-        in_instruction_write = 0;
-        in_instruction = 12'b001100000110;
-        in_instruction_address = 11;
-        in_instruction_write = 1;
+		//Instruction end
 	end
 	
 	always @(negedge KEY[1]) in_running <= 1;
 	
 	assign LEDG[0] = out_running;
+
+	assign in_input[3:0] = KEY[3:0];
 
 endmodule
